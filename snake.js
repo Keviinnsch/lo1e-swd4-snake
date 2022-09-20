63465fd03addf085afe76c284d9eadbc4dc5dbfe
Keviinnsch <6002490@mborijnland.nl>
2022-09-20 13:48:32 +0200

    code was weg, dus geupdate

--- snake.js
+++ snake.js
@@ -1,51 +1,75 @@
 let canvas = document.getElementById("snake");
-let context = canvas.getContext("2d");
+let ctx = canvas.getContext("2d");
 
 let snakeX = 200;
 let snakeY = 200;
-
 let direction = null;
 
-// Draw the background
 function drawBackground() {
-    context.fillStyle = "black";
-    context.fillRect(0, 0, 400, 400);
+ctx.fillStyle = "black";
+ctx.fillRect(0, 0, 400, 400);
 }
 
-// Draw the snake
-function drawSnake() {
-    context.fillStyle = "Red";
-    context.fillRect(snakeX, snakeY, 20, 20);
+
+function drawSnake(){
+ctx.fillStyle = "Red";
+ctx.fillRect(snakeX, snakeY, 20, 20)
 }
 
 function update() {
-    if (direction == "right") {
-        snakeX += 20;
-    } else if (direction == "left") {
-        snakeX -= 20;
-    } else if (direction == "up") {
-        snakeY -= 20;
-    } else if (direction == "down") {
-        snakeY += 20;
-    }
+    if(direction == 'right') {
+        if (snakeX < 380) {
+            snakeX += 20;
+        }else {
+            gameOver();
+        }
+    }else if(direction == 'left') {
+        if (snakeX > 0) {
+            snakeX -= 20;
+        }else {
+            gameOver();
+        }
 
+    }else if(direction == 'down') {
+        if (snakeY < 380) {
+            snakeY += 20;
+        }else {
+            gameOver();
+        }
+    }else if(direction == 'up') {
+        if (snakeY > 0) {
+            snakeY -= 20;
+        }else {
+            gameOver();
+        }
+    }
     drawBackground();
     drawSnake();
 }
 
 function changeDirection(event) {
-    if (event.code == "ArrowUp") {
-        direction = "up";
-    } else if (event.code == "ArrowRight") {
-        direction = "right";
-    } else if (event.code == "ArrowLeft") {
-        direction = "left";
-    } else if (event.code == "ArrowDown") {
-        direction = "down";
+    if(event.code == 'ArrowUp' ) {
+    direction = 'up';
     }
+    if(event.code == 'ArrowRight' ) {
+    direction = 'right';
+    }
+    if(event.code == 'ArrowLeft' ) {
+    direction = 'left';
+    }
+    if(event.code == 'ArrowDown' ) {
+    direction = 'down';
+    }
+}
+
+
+function gameOver() {
+    direction = null;
+    alert("Game Over!")
 }
 
 drawBackground();
 drawSnake();
-setInterval(update, 1000);
-addEventListener("keydown", changeDirection);
+
+setInterval(update, 200);
+addEventListener('keydown', changeDirection)
